2d9af3802ba0c5786ad1b872a0123396be569cf1
Oliver Lohoff <info@contao4you.de>
2025-06-28 15:22:11 +0200

    Nur eine Kategorie / Root-Page

--- contao/dca/tl_module.php
+++ contao/dca/tl_module.php
@@ -3,17 +3,17 @@
 use C4Y\SearchLiteBundle\EventListener\DataContainer\SearchLiteModuleListener;
 
 $GLOBALS['TL_DCA']['tl_module']['palettes']['search_module'] =
-    '{title_legend},name,type,search_lite_categories,search_lite_rootPages'
+    '{title_legend},name,type,search_lite_category,search_lite_rootPage'
 ;
 
-$GLOBALS['TL_DCA']['tl_module']['fields']['search_lite_categories'] = array(
+$GLOBALS['TL_DCA']['tl_module']['fields']['search_lite_category'] = array(
     'inputType' => 'select',
     'eval' => array('tl_class' => 'clr'),
     'foreignKey' => 'tl_search_lite_featured_categories.title',
     'sql' => "int(10) unsigned NOT NULL default 0"
 );
 
-$GLOBALS['TL_DCA']['tl_module']['fields']['search_lite_rootPages'] = array(
+$GLOBALS['TL_DCA']['tl_module']['fields']['search_lite_rootPage'] = array(
     'inputType' => 'select',
     'eval' => array('tl_class' => 'clr'),
     'options_callback' => [SearchLiteModuleListener::class, 'getRootPages'],
--- contao/languages/de/tl_module.php
+++ contao/languages/de/tl_module.php
@@ -1,11 +1,11 @@
 <?php
 
-$GLOBALS['TL_LANG']['tl_module']['search_lite_categories'] = [
+$GLOBALS['TL_LANG']['tl_module']['search_lite_category'] = [
     'Suche - Top-Treffer',
     '',
 ];
 
-$GLOBALS['TL_LANG']['tl_module']['search_lite_rootPages'] = [
+$GLOBALS['TL_LANG']['tl_module']['search_lite_rootPage'] = [
     'Suche - Root-Seite',
     'In welchem Seitenbaum soll gesucht werden?',
 ];
