c61c9099998e92e34c1be08074b5df0de10a62ba
Oliver Lohoff <info@contao4you.de>
2026-01-28 17:55:11 +0100

    Package Namen geändert

--- src/Service/LoupeEngineFactory.php
+++ src/Service/LoupeEngineFactory.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @package    contao-search-lite
+ * @package    search4you
  * @author     Oliver Lohoff <info@contao4you.de>
  * @copyright  Contao4you 2025
  * @license    LGPL-3.0-or-later
